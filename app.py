88558103bc9efb92e4d893932d55568b6a4a08d4
Masaki MURATA <masaki@MasakinoMacBook-Pro.local>
2022-09-14 15:37:16 +0900

    cabochaを導入し，20文字を超える行は直前で改行を行うようにした．また，字幕の表示行数を5行にする処理を加えた．

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from datetime import timedelta
 
-import ipadic
-import MeCab
+import CaboCha
 from flask import Flask  # Flaskと、HTMLをレンダリングするrender_templateをインポート
 from flask import Markup, render_template, request, session
 
 app = Flask(__name__)  # Flask の起動
 app.secret_key = 'secret'
+# session は 3 分で破棄
 app.permanent_session_lifetime = timedelta(minutes=3)
 
-
-t = MeCab.Tagger(f'-O wakati {ipadic.MECAB_ARGS}')
+c = CaboCha.Parser()
 
 
 @app.route('/')  # localhost:50000/を起動した際に実行される
@@ -30,31 +29,52 @@ def show_iframe():
         fontweight=900,
         stylecolor="#ffffff",
         stcolor="#000000",
-        linefeed_text=''
+        linefeed_text="[ここに結果表示（音声認識）]"
     )
 
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    # index.htmlのinputタグ内にあるname属性itemを取得し、textに格納した
+    # session に保存してあるテキストを読み込み
     text = session.get('text', '')
-    text += request.form.get('item')
+    # index.htmlのinputタグ内にあるname属性recog_textを取得し、textに結合
+    text += request.form.get('recog_text')
+    # session に保存
     session['text'] = text
-    text_parse = t.parse(text)
+
+    # cabocha で解析
+    tree = c.parse(text)
 
     length = 0
     linefeed_text = ''
-    for token in text_parse.split(' '):
-        length += len(token)
-        if length > 20:
-            linefeed_text += "<br>"
-            length -= 20
+    for i in range(tree.chunk_size()):  # 文節数のループ
+        chunk = tree.chunk(i)  # chunk=文節．i 番目の文節
 
-        linefeed_text += token
+        chunk_text = ''
+        # 文節内の各形態素解析結果を表示
+        for ix in range(chunk.token_pos, chunk.token_pos + chunk.token_size):
+            token = tree.token(ix)
+            # token.normalized_surface  # 表層
+            # token.feature  # 形態素情報
+
+            chunk_text += token.normalized_surface  # 形態素ごとに繋いでいく
+
+        print(chunk_text)
+        length += len(chunk_text)
+        if length > 20:  # 最終文節を繋いだときに20文字を超えていたら
+            linefeed_text += "<br>"  # 直前の文節境界に改行を付与
+            length = len(chunk_text)
+
+        linefeed_text += chunk_text
+    print(linefeed_text)
+
+    # 字幕の表示は5行（6行以上になったら5行にカット）
+    if linefeed_text.count("<br") >= 5:
+        linefeed_text = "<br>".join(linefeed_text.split("<br>")[1:])
+    print(linefeed_text)
 
     # もしPOSTメソッドならresult.htmlに値textと一緒に飛ばす
     if request.method == 'POST':
-        print(text)
         return render_template(
             "main.html",
             bgcolor="#00ff00",
