9005dcea0c9ad48431ac1413f9c6b9af6383841b
Masaki MURATA <masaki@MasakinoMacBook-Pro.local>
2022-09-16 17:12:49 +0900

    改行挿入処理の追加 #1

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ import CaboCha
 import torch
 from flask import Flask  # Flaskと、HTMLをレンダリングするrender_templateをインポート
 from flask import Markup, render_template, request, session
-from transformers import AutoModelForMaskedLM, AutoTokenizer
+from transformers import BertTokenizer
+
+from model import BERTClass, load_best
 
 # GPU on M1 Mac
 device = torch.device("mps")
-tokenizer = AutoTokenizer.from_pretrained("cl-tohoku/bert-base-japanese-whole-word-masking")
-model = AutoModelForMaskedLM.from_pretrained("cl-tohoku/bert-base-japanese-whole-word-masking")
+tokenizer = BertTokenizer.from_pretrained(
+    "cl-tohoku/bert-base-japanese-whole-word-masking"
+)
+model = BERTClass()
 model.to(device)
+best_model = './insertion_model.pt'
+model = load_best(best_model, model)
 
 app = Flask(__name__)  # Flask の起動
 
@@ -29,6 +35,7 @@ def lf_and_punc_insertion(text):
 
     length = 0
     linefeed_text = ''
+    old_chunk_text = ''
     for i in range(tree.chunk_size()):  # 文節数のループ
         chunk = tree.chunk(i)  # chunk=文節．i 番目の文節
 
@@ -42,12 +49,45 @@ def lf_and_punc_insertion(text):
             chunk_text += token.normalized_surface  # 形態素ごとに繋いでいく
 
         print(chunk_text)
+
         length += len(chunk_text)
-        if length > 20:  # 最終文節を繋いだときに20文字を超えていたら
+
+        test_text = old_chunk_text + "[SEP]" + chunk_text
+        with torch.no_grad():
+            inputs = tokenizer.encode_plus(
+                test_text,
+                None,
+                add_special_tokens=True,
+                max_length=50,
+                padding='max_length',
+                return_token_type_ids=True,
+                truncation=True
+            )
+            ids = inputs['input_ids']
+            mask = inputs['attention_mask']
+            token_type_ids = inputs["token_type_ids"]
+            ids = torch.tensor([ids], dtype=torch.long).to(device, dtype=torch.long)
+            mask = torch.tensor([mask], dtype=torch.long).to(device, dtype=torch.long)
+            token_type_ids = torch.tensor([token_type_ids], dtype=torch.long).to(device, dtype=torch.long)
+
+            # print(ids)  # 読み込んだテキストを id 化したものの確認
+
+            output = model(ids, mask, token_type_ids)
+
+        pred = torch.sigmoid(output).cpu().detach().numpy().tolist()
+        print(pred[0][0])
+        if pred[0][0] > 0.5:
+            print("モデルによる改行")
+            linefeed_text += "<br>"  # 直前の文節境界に改行を付与
+            length = len(chunk_text)
+        elif length > 20:  # 最終文節を繋いだときに20文字を超えていたら
+            print("文字数による改行")
             linefeed_text += "<br>"  # 直前の文節境界に改行を付与
             length = len(chunk_text)
 
         linefeed_text += chunk_text
+
+        old_chunk_text = chunk_text
 
     print(linefeed_text)
 
@@ -79,27 +119,26 @@ def show_iframe():
 def result():
     # session に保存してあるテキストを読み込み
     text = session.get('text', '')
-    # index.htmlのinputタグ内にあるname属性recog_textを取得し、textに結合
-    text += request.form.get('recog_text')
-    # session に保存
-    session['text'] = text
 
-    linefeed_text = lf_and_punc_insertion(text)
+    text_split = text.split('<br>')  # 改行<br>で分割
+    pre = "<br>".join(text_split[:-1]) + "<br>"  # 最終の改行までの文字列（確定済み）
+    now = text_split[-1]  # 最終の改行以降の文字列
 
-    # BERT
-    input_ids = tokenizer.encode(linefeed_text, return_tensors='pt').to(device)
-    print(input_ids)
+    # index.htmlのinputタグ内にあるname属性recog_textを取得し、nowに結合
+    now += request.form.get('recog_text')
 
-    with torch.no_grad():
-        output = model(input_ids = input_ids)
-        scores = output.logits
-    print(scores)
+    # 改行挿入
+    linefeed_text = pre + lf_and_punc_insertion(now)
 
     # 字幕の表示は5行（6行以上になったら5行にカット）
-    if linefeed_text.count("<br") >= 5:
-        linefeed_text = "<br>".join(linefeed_text.split("<br>")[1:])
+    if linefeed_text.count("<br>") >= 5:
+        linefeed_text = "<br>".join(linefeed_text.split("<br>")[-5:])
     print(linefeed_text)
 
+    # 改行挿入結果を session に保存
+    session['text'] = linefeed_text
+
+    # 表示
     return render_template(
         "main.html",
         bgcolor="#00ff00",
